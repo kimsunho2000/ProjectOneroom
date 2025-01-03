88b1b361c450d5cb13bea5378ba555d2763bfcbb
kimsunho2000 <sunhokim28@gmail.com>
2025-01-03 16:24:54 +0900

    Initial Commit

---
+++ OneRoom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneRoom
+//
+//  Created by 김선호 on 1/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OneRoomTests/OneRoomTests.swift
@@ -0,0 +1,17 @@
+//
+//  OneRoomTests.swift
+//  OneRoomTests
+//
+//  Created by 김선호 on 1/3/25.
+//
+
+import Testing
+@testable import OneRoom
+
+struct OneRoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
