90f510b8322db040d8a91ca24a50e27efca3f57f
kimsunho2000 <sunhokim28@gmail.com>
2025-01-26 22:24:25 +0900

    프로젝트 디렉토리 구조 변경

--- OneRoom/Networks/DatabaseManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DatabaseManager.swift
-//  OneRoom
-//
-//  Created by 김선호 on 1/10/25.
-//
-
-import Foundation
--- OneRoom/Networks/LoginAuth.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LoginAuth.swift
-//  OneRoom
-//
-//  Created by 김선호 on 1/26/25.
-//
-
-import Foundation
